0ad62b0185551ba9aefd6a2a80e653c72570fad9
tejas-vivek <tejas.helwatkar@gmail.com>
2025-06-11 16:01:12 -0400

    Add mongoDB models for user and subcription

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { PORT } from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import authRouter from "./routes/auth.routes.js";
+import connectToDatabase from "./database/monogodb.js";
 
 const app = express();
 
@@ -14,8 +15,10 @@ app.get("/", (req, res) => {
     res.send("Welcome to the subscription tracker API!");
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, async() => {
     console.log(`Server is running on http://localhost:${PORT}`);
+
+    await connectToDatabase();
 })
 
 export default app;
